4c9daa59143b79d4a77145e9f1fbe92e3dec0192
Soroush Saffari <tools@soroushsaffari.com>
2020-06-18 13:00:57 +0100

    Home: draft

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './scenes/home/index.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <HomePage />
     </div>
   );
 }
